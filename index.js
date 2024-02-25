ce2fc6d0b29e2f1d666068e36513c93d560e61b6
sara rodriguez <sararbd25@gmail.com>
2024-02-25 13:45:13 -0500

    Implementacion de opciones de menu y la funcion para operar restas

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Función para sumar dos números
-function sumar(num1, num2) {
-    return num1 + num2;
+function restar(num1, num2) {
+    return num1 - num2;
 }
 
   // Función principal del programa
